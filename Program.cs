23a9b28beb9f561958600b458f5ad93669907784
Abel Martínez Hernández <rainbowsix_mha@hotmail.com>
2021-07-23 19:28:58 -0500

    mensaje a mamarre

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Industrial_Complements_Code
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Puto miki");
+            Console.WriteLine("el mamarre");
         }
     }
 }
