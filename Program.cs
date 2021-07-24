d92876e0f5c5b4dc8f3b54f79511fe89b8288102
Abel Martínez Hernández <rainbowsix_mha@hotmail.com>
2021-07-23 20:03:37 -0500

    borado de lineas

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace Industrial_Complements_Code
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("chupala mamarre");
-            Console.WriteLine("mamarre mamarre mamarre");
+           
+         
         }
     }
 }
