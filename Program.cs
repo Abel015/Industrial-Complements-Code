72d1fd9d8af2a03cbf6534a069e79cbb3fdd62d5
Abel Martínez Hernández <rainbowsix_mha@hotmail.com>
2021-07-23 19:52:00 -0500

    borrado primer write line

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ namespace Industrial_Complements_Code
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("el mamarre 2");
             Console.WriteLine("chupala mamarre");
             Console.WriteLine("mamarre mamarre mamarre");
         }
