04794c61a7644307071284aed8d2536713ff0b52
Eduardo Cruz <eduardoxcruz2000@hotmail.com>
2021-07-23 19:42:14 -0500

    otro cambio

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Industrial_Complements_Code
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("el mamarre");
+            Console.WriteLine("chupala mamarre");
             Console.WriteLine("mamarre mamarre mamarre");
         }
     }
